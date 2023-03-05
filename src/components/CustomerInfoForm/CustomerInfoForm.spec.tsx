2f46e1ae4f93b409725fa095d37f7040a760a1a7
mydatahack <thon6thon6@hotmail.com>
2023-03-06 09:00:29 +1100

    Update unit test comments

--- src/components/CustomerInfoForm/CustomerInfoForm.spec.tsx
+++ src/components/CustomerInfoForm/CustomerInfoForm.spec.tsx
@@ -14,10 +14,17 @@ describe('<CustomerInfoForm />', () => {
   it('should render the form and submit', async () => {
     render(<CustomerInfoForm />)
     expect(screen.getByTestId('customer-info-form')).toBeVisible()
+    // This results in console.error:
+    // Warning: An update to Formik inside a test was not wrapped in act(...).
+    // When testing, code that causes React state updates should be wrapped into act(...):
+    // userEvent.type(screen.getByLabelText('First Name'), 'FirstName')
+    // So, this works
     await act(async () => {
       await userEvent.type(screen.getByLabelText('First Name'), 'FirstName')
     })
 
+    // Same as above
+    // userEvent.click(screen.getByRole('button', { name: 'Submit' }))
     await act(async () => {
       await userEvent.click(screen.getByRole('button', { name: 'Submit' }))
     })
